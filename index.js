178e826e52dfd8c76651bf8924446f6b5ce181cb
Dev Mandal <devmandalgps@gmail.com>
2026-09-17 12:54:04 +0530

    Add EJS views and static styling

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 const port = 3000;
 
-// // Set views folder
-// app.set("views", path.join(__dirname, "views"));
-
-// // Set EJS as template engine
-// app.set("view engine", "ejs");
-
-// // Home route
-// app.get("/", (req, res) => {
-//     res.render("home");
-// });
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.send("Server is running");
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username: "Dev mandal",
+        content: "This is my first post!"
+    },
+    {
+        username: "Adersh N",
+        content: "I am a Mining Engineer!"
+    },
+    {
+        username: "Ganesh N",
+        content: "I got a internship at Qspider."
+    }
+];
+
+app.get("/posts", (req, res) => {
+    res.render("home", { posts: posts });
 });
 
-// Start server
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
