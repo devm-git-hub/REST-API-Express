f651cd40e40a46dd3b34cde5f08a4a1b47fa9f59
Dev Mandal <devmandalgps@gmail.com>
2026-09-22 12:19:02 +0530

    Redirect

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts", (req, res) => {
     const { username, content } = req.body;
     posts.push({ username, content });
+    
     res.redirect("/posts");
 });
 
